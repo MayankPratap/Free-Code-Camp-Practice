0d453ad5979165b0a42f8cebb6e6a891ac55d5fd
Mayank Pratap Singh <MayankPratap@users.noreply.github.com>
2016-06-06 16:06:50 +0530

    Added code for Where Art Thou

---
+++ IntermediateAlgorithmChallenges/whereartthou.js
@@ -0,0 +1,47 @@
+function whereAreYou(collection, source) {
+  // What's in a name?
+  var arr = [];
+  // Only change code below this line
+  
+  for(var idx in collection){
+    
+     var obj=collection[idx];
+     
+    
+       // console.log(key+" "+obj[key]);    
+         
+       var flag=1;
+      for(var name in source){
+          
+          if(obj.hasOwnProperty(name) && obj[name]==source[name]){
+             
+             console.log(obj[name]);   
+          }
+        
+          else{
+          
+           flag=0;
+          }
+          
+               
+       }
+    
+    if(flag==1){
+      
+      // console.log("Yes");
+       arr.push(obj);
+       
+       
+    } 
+       
+    
+   
+    
+  }
+  
+  
+  // Only change code above this line
+  return arr;
+}
+
+whereAreYou([{ first: "Romeo", last: "Montague" }, { first: "Mercutio", last: null }, { first: "Tybalt", last: "Capulet" }], { last: "Capulet" });
