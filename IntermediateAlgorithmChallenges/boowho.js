44efd151313c5f11b50457e1f8bee5f5cdbbdb0f
Mayank Pratap Singh <MayankPratap@users.noreply.github.com>
2016-06-07 18:19:16 +0530

    Check if value is boolean primitive

---
+++ IntermediateAlgorithmChallenges/boowho.js
@@ -0,0 +1,13 @@
+
+function booWho(bool) {
+  // What is the new fad diet for ghost developers? The Boolean.
+  if(bool===0 || bool===-0 || bool===null || isNaN(bool) || bool===undefined || bool==="" || bool===1)
+     return false;
+  
+  else
+     return true;
+     
+   
+}
+
+booWho(null);
